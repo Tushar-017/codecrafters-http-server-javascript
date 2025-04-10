1b3b0fe8461710e92fd7f3dbbd2550254a1213c0
Tushar Rajput <tusharo17rajput@gmail.com>
2025-04-10 21:55:00 +0530

    updated the if condition

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ console.log("Logs from your program will appear here!")
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    let responseStatus = "200 OK"
+    let responseStatus = "404 Not Found"
     let contentType = ""
     let contentLength = ""
     let body = ""
@@ -14,13 +14,14 @@ const server = net.createServer((socket) => {
     const [method, path, _] = request.split(" ")
 
     if (method === "GET") {
-      if (path !== "/" && !path.includes("/echo")) {
-        responseStatus = "404 Not Found"
-      } else if (path.includes("/echo")) {
+      if (path === "/") {
+        responseStatus = "200 OK"
+      } else if (path.startsWith("/echo/")) {
         const param = path.split("/")[2]
+        responseStatus = "200 OK"
         contentType = "Content-Type: text/plain"
-        contentLength = param?.length && `Content-Length: ${param.length}` // Fixed capitalization of 'Length'
-        body = param && param + "\r\n"
+        contentLength = `Content-Length: ${param.length}`
+        body = param + "\r\n"
       }
     }
 
