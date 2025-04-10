9303281acd6e5a8fda48ff0ef636914e54b35f4c
Tushar Rajput <tusharo17rajput@gmail.com>
2025-04-10 21:48:48 +0530

    handled echo route with param

--- app/main.js
+++ app/main.js
@@ -6,17 +6,34 @@ console.log("Logs from your program will appear here!")
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
+    let responseStatus = "200 OK"
+    let contentType = ""
+    let contentLength = ""
+    let body = ""
     const request = data.toString()
-    let responseStatus
     const [method, path, _] = request.split(" ")
-    if (method === "GET") {
-      responseStatus = path === "/" ? "200 OK" : "404 Not Found"
-    }
-    socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`)
 
-    socket.on("close", () => {
-      socket.end()
-    })
+    if (method === "GET") {
+      if (path !== "/" && !path.includes("/echo")) {
+        responseStatus = "404 Not Found"
+      } else if (path.includes("/echo")) {
+        const param = path.split("/")[2]
+        contentType = "Content-Type: text/plain"
+        contentLength = `Content-length: ${param?.length}`
+        body = param && param + "\r\n"
+      }
+    }
+
+    const headers = [contentType, contentLength].filter(Boolean).join("\r\n")
+    const response = [`HTTP/1.1 ${responseStatus}`, headers, "", body]
+      .filter(Boolean)
+      .join("\r\n")
+
+    socket.write(response)
+    socket.end()
+  })
+  socket.on("close", () => {
+    socket.end()
   })
 })
 
